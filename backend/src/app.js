76655f6f91c936e4ad1f9c0353c31d299011f39d
jay finaviya <jayfinaviya5@gmail.com>
2026-05-09 19:47:04 +0530

    Add Project Files

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/SocketManager.js";
+
+import cors from "cors";
+
+import userRoutes from "./routes/users.routes.js";
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port",(process.env.PORT || 8000))
+
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb",extended: true}));
+
+app.use("/api/v1/users",userRoutes);
+
+const start = async()=>{
+    const connectionDb = await mongoose.connect("mongodb+srv://VideoCall:video123@cluster0.1nvj7uw.mongodb.net/");
+    console.log(`mongo Connected DB Host:${connectionDb.connection.host}`)
+    server.listen(app.get("port"),() =>{
+        console.log("Listen on port 8000");
+    });
+}
+
+start();
---
+++ backend/src/controllers/SocketManager.js
@@ -0,0 +1,32 @@
+import { sign } from "crypto";
+import { Socket } from "dgram";
+import { send } from "process";
+import { Server} from "socket.io";
+
+
+let connections = {}
+let messages = {}
+let timeOnline = {}
+
+export const connectToSocket = (server) =>{
+    const io = new Server(server);
+
+    io.on("connection",(socket)=>{
+         
+        socket.on("join-call", (path)=>{
+
+        })
+        socket.on("signal",(toId,message)=>{
+            io.to(toId).emit("signal",socket.id, message);
+        })
+
+        socket.on("chat-message",(data, sender)=>{
+
+        })
+        socket.on("disconnect",()=> {
+            
+        })
+    })
+
+    return io;
+}
