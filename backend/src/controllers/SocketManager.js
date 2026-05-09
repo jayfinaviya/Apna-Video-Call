03ed2594ebb1e3b79fe5adef39eb1752eb3dd100
jay finaviya <jayfinaviya5@gmail.com>
2026-05-09 20:10:48 +0530

    Add Project Files

--- backend/src/controllers/SocketManager.js
+++ backend/src/controllers/SocketManager.js
@@ -14,6 +14,28 @@ export const connectToSocket = (server) =>{
     io.on("connection",(socket)=>{
          
         socket.on("join-call", (path)=>{
+          
+            if (connections[path] === undefined) {
+                connections[path] = []
+            }
+            connections[path].push(socket.id)
+
+            timeOnline[socket.id] = new Date();
+
+            // connections[path].forEach(elem => {
+            //     io.to(elem)
+            // })
+
+            for (let a = 0; a < connections[path].length; a++) {
+                io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
+            }
+
+            if (messages[path] !== undefined) {
+                for (let a = 0; a < messages[path].length; ++a) {
+                    io.to(socket.id).emit("chat-message", messages[path][a]['data'],
+                        messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
+                }
+            }
 
         })
         socket.on("signal",(toId,message)=>{
@@ -24,7 +46,7 @@ export const connectToSocket = (server) =>{
 
         })
         socket.on("disconnect",()=> {
-            
+
         })
     })
 
