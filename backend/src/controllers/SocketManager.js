7d17b4e8ee4749947962536090506482f9ab4f6d
jay finaviya <jayfinaviya5@gmail.com>
2026-05-09 20:14:57 +0530

    Add Project Files

--- backend/src/controllers/SocketManager.js
+++ backend/src/controllers/SocketManager.js
@@ -73,7 +73,32 @@ export const connectToSocket = (server) =>{
         })
         socket.on("disconnect",()=> {
 
-            
+           var diffTime = Math.abs(timeOnline[socket.id] - new Date())
+
+            var key
+
+            for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
+
+                for (let a = 0; a < v.length; ++a) {
+                    if (v[a] === socket.id) {
+                        key = k
+
+                        for (let a = 0; a < connections[key].length; ++a) {
+                            io.to(connections[key][a]).emit('user-left', socket.id)
+                        }
+
+                        var index = connections[key].indexOf(socket.id)
+
+                        connections[key].splice(index, 1)
+
+
+                        if (connections[key].length === 0) {
+                            delete connections[key]
+                        }
+                    }
+                }
+
+            }  
 
         })
     })
