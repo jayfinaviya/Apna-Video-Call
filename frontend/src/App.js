645c8fc079a6be0fabcc2f4332fc720a05f181bc
jay finaviya <jayfinaviya5@gmail.com>
2026-05-15 13:32:10 +0530

    Add Project Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LandingPage from './pages/landing.jsx';
+import LandingPage from './pages/Landing.jsx';
 import Authentication from './pages/authentication.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import VideoMeetComponent from './pages/VideoMeet.jsx';
