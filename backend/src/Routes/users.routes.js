cb5fa18470f731c85e2bba1fe4e82f44c3bd8b58
jay finaviya <jayfinaviya5@gmail.com>
2026-05-15 12:40:01 +0530

    Add Project Files

--- backend/src/Routes/users.routes.js
+++ backend/src/Routes/users.routes.js
@@ -10,4 +10,4 @@ router.route("/register").post(register)
 router.route("/add_to_activity").post(addToHistory)
 router.route("/get_all_activity").get(getUserHistory)
 
-export default 
+export default router;
